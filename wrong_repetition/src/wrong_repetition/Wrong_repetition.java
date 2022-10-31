86a701ab2f994ef44a261386d2f983384d718973
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-31 21:16:39 +0200

    implemented wrong_repetition detection functionality

--- wrong_repetition/src/wrong_repetition/Wrong_repetition.java
+++ wrong_repetition/src/wrong_repetition/Wrong_repetition.java
@@ -18,7 +18,29 @@ public class Wrong_repetition {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        Scanner myScanner = new Scanner(System.in);
         
+        System.out.print("Enter a sentence: ");
+        
+        String input = myScanner.nextLine();
+
+        String[] inputArr = input.split(" ");
+        
+        String output = input;
+        
+        for(int i = 0; i < inputArr.length-1; i++){
+            
+            if(inputArr[i].equals(inputArr[i+1])){
+                output = output.replaceFirst(inputArr[i], "");
+            }
+        }
+        
+        if(!input.equals(output)){
+            System.out.println("The sentence includes wrong repetitions.");
+            System.out.println("The sentence should be: "+ output);
+        }else{
+            System.out.println("There are no wrong repetitions");
+        }
     }
     
 }
