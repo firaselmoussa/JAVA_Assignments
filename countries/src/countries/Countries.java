84cbacda2e1ff9acb6044fe76880c23f90f7f006
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-31 19:36:46 +0200

    implemented text sample

--- countries/src/countries/Countries.java
+++ countries/src/countries/Countries.java
@@ -16,6 +16,12 @@ public class Countries {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        Country LB = new Country("Lebanon", "Asia", 10452, 5882000);
+        Country FR = new Country("France", "Europe", 640679, 66616416);
+        Country MX = new Country("Mexico", "America", 1972550, 118395054);
+        
+        System.out.println(LB.toString() +"\n"+ FR.toString() +"\n"+ MX.toString());
+
     }
     
 }
