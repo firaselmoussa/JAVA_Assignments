57751ccb76a1c0189fc05ff1617abeb286757a80
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-31 18:23:53 +0200

    implemented COUNTRY Constructor

---
+++ countries/src/countries/Country.java
@@ -0,0 +1,47 @@
+package countries;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Firas
+ */
+public class Country {
+    
+    private String name, countryCode, continent;
+    private double area;
+    private int population;
+    
+    private static final Map<String, String> continents = new HashMap<>();
+    static {
+        continents.put("Africa", "1");
+        continents.put("America", "2");
+        continents.put("Asia", "3");
+        continents.put("Oceania", "4");
+        continents.put("Europe", "5");
+    }
+    
+    
+    public Country( String givenName,
+                    String givenContinent,
+                    double givenArea,
+                    int givenPopulation ){
+        
+        name = givenName;
+        continent = givenContinent;
+        area = givenArea;
+        population = givenPopulation;
+        
+        String[] nameArr = name.split("");
+   
+        countryCode = nameArr[0]+nameArr[1]+nameArr[2]+'-'+continents.get(givenContinent);
+
+    }
+ 
+}
