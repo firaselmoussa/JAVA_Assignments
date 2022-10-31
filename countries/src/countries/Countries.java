fc970bbf9428d1db6d988030a60bfe783aa1edeb
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-31 19:38:09 +0200

    added comments

--- countries/src/countries/Countries.java
+++ countries/src/countries/Countries.java
@@ -16,10 +16,12 @@ public class Countries {
     public static void main(String[] args) {
         // TODO code application logic here
         
+//        creating 3 Country objects
         Country LB = new Country("Lebanon", "Asia", 10452, 5882000);
         Country FR = new Country("France", "Europe", 640679, 66616416);
         Country MX = new Country("Mexico", "America", 1972550, 118395054);
         
+//        printing each objects data by calling tostring method
         System.out.println(LB.toString() +"\n"+ FR.toString() +"\n"+ MX.toString());
 
     }
