b00b68ee99d6be8e032356056c49bc400494c6a1
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-31 21:20:08 +0200

    added comments

--- wrong_repetition/src/wrong_repetition/Wrong_repetition.java
+++ wrong_repetition/src/wrong_repetition/Wrong_repetition.java
@@ -18,23 +18,31 @@ public class Wrong_repetition {
     public static void main(String[] args) {
         // TODO code application logic here
         
+//        created Scanner
         Scanner myScanner = new Scanner(System.in);
         
+//        prompted user to input a sentence
         System.out.print("Enter a sentence: ");
         
+//        store user input
         String input = myScanner.nextLine();
 
+//        split user input into array of words
         String[] inputArr = input.split(" ");
         
+//        creating an output version of the users input
         String output = input;
         
+//        looping over each word of the users input 
         for(int i = 0; i < inputArr.length-1; i++){
             
+//            comparing each word with the word that comes next, if they match one gets removed 
             if(inputArr[i].equals(inputArr[i+1])){
                 output = output.replaceFirst(inputArr[i], "");
             }
         }
         
+//        printing out the result accordingly 
         if(!input.equals(output)){
             System.out.println("The sentence includes wrong repetitions.");
             System.out.println("The sentence should be: "+ output);
