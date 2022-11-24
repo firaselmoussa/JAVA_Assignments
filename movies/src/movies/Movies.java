79ff3f8bb31c96f15f093d0c2700cb06be5d9c3d
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-24 18:37:54 +0200

    created new movie object with a director

--- movies/src/movies/Movies.java
+++ movies/src/movies/Movies.java
@@ -15,6 +15,20 @@ public class Movies {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Director director = new Director("Alex", "Spanish", 25, "Male", false);
+        
+        Actor actors[] = { new Actor("John", "American", 58, "Male", false, true), 
+                           new Actor("Emily", "French", 24, "Female", false, true),
+                           new Actor("Tina", "American", 44, "Female", false, false), 
+                           new Actor("Leon", "American", 31, "Male", false, false),
+                           new Actor("Joanna", "British", 43, "Female", false, false), 
+                           new Actor("Hugh", "Irish", 69, "Male", false, false)};
+        
+        Movie movie = new Movie("hobs", "fantasy", director, actors);
+        
+
+
     }
     
 }
