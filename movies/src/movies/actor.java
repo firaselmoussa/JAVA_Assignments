9332b1d87880cfb9f7e415c8644816744eeab6c2
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-24 17:42:37 +0200

    implemented actor constructor

--- movies/src/movies/actor.java
+++ movies/src/movies/actor.java
@@ -8,12 +8,20 @@ package movies;
  *
  * @author Firas
  */
-public class actor {
+public class Actor {
     private String name;
     private String nationality;
     private int age;
     private String gender;
     private boolean isRetired;
+    private boolean isMainCharacter;
     
-    
+    public Actor(String name, String nationality, int age, String gender, boolean isRetired, boolean isMainCharacter){
+        this.name = name;
+        this.nationality = nationality;
+        this.age = age;
+        this.gender = gender;
+        this.isRetired = isRetired;
+        this.isMainCharacter = isMainCharacter;
+    }
 }
