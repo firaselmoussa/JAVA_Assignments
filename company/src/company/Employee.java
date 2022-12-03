9a83c25f9325035cd3d7b376dd7c6258275acdbb
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-12-03 14:39:29 +0200

    implemented data fields & constructor

--- company/src/company/Employee.java
+++ company/src/company/Employee.java
@@ -9,5 +9,14 @@ package company;
  * @author Firas
  */
 public class Employee {
+    String firstName, lastName;
+    int age, id;
     
+    public Employee(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        
+        id = firstName.charAt(0)+lastName.charAt(0)+'#'+age;
+    }
 }
