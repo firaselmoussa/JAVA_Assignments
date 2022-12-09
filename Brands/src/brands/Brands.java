aae00705c3824da4e8977b5e50c03c6eac17a220
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-12-09 20:33:50 +0200

    added comments

--- Brands/src/brands/Brands.java
+++ Brands/src/brands/Brands.java
@@ -18,20 +18,24 @@ public class Brands {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        //CREATED BRAND OBJECT 
         Brand GeneralMotors = new Brand("General Motors", "Michigan");
         GeneralMotors.setNumberOfProducts(30); // number of products is being set prior to creating a new product which will add the products number dynamically 
-        
+        //CREATED BRAND OBJECT 
         Brand Apple = new Brand("Apple", "California");
         Apple.setNumberOfProducts(500); // number of products is being set prior to creating a new product which will add the products number dynamically 
    
+        //CREATED ARRAY LIST OF PRODUCTS
         ArrayList<Product> products = new ArrayList<>();
         
+        //CREATED PRODUCT OBJECTS & ADDED THEM TO THE ARRAY LIST
         products.add(new Product("Chevrolet trax 2022", "Veihcle", GeneralMotors));
         products.add(new Product("Iphone 14", "smartphonr", Apple));
-        
+        //SETTING PRODUCT'S QUALITY
         products.get(0).setQuality("Fast");
         products.get(1).setQuality("reliable");
         
+        //DISPLAYING DATA
         for(int i = 0; i < products.size(); i++){
             System.out.println(products.get(i).toString());
         }
