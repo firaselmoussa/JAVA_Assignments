e44d0325a3bb5d4e90de0978cfaae2bc5636caa2
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-12-10 20:33:50 +0200

    fixed typo bug

--- Bank/src/bank/CarLoan.java
+++ Bank/src/bank/CarLoan.java
@@ -44,7 +44,7 @@ public class CarLoan extends Loan{
     
     
     //FINAL AMOUNT METHOD
-    public double finalAmount(double tax){
+    public double finalAmount(){
         return super.getLoanAmount()+insuranceFee;
     }
 
