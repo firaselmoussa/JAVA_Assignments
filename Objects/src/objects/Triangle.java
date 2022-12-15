2cb42ca5b2a63b47c9988b77e51704452ddc443b
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-12-15 18:25:48 +0200

    implemented constructor

--- Objects/src/objects/Triangle.java
+++ Objects/src/objects/Triangle.java
@@ -8,12 +8,19 @@ package objects;
  *
  * @author Firas
  */
-public class Triangle {
+public class Triangle extends GeometricObject{
     private double side1, side2, side3 = 1.0;
     private double area, perimeter;
     
     public Triangle() {
         super();
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+    
+    public Triangle(double side1, double side2, double side3, String color, boolean isFilled) {
+        super(color, isFilled);
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
