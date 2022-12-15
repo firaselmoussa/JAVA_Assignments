d8154e56b4debaf74d5aff17d3cdb6ecb420698c
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-12-15 18:39:42 +0200

    implemented user prompt

--- Objects/src/objects/Objects.java
+++ Objects/src/objects/Objects.java
@@ -1,19 +1,40 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package objects;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
  * @author Firas
  */
 public class Objects {
-
     /**
      * @param args the command line arguments
      */
+ 
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        String loop_termination;
+        
+        do{
+            
+            System.out.println("NEW TRIANGLE\n"
+                    + "---------------------------------------------------------"
+                    + "Choose color: ");
+            String color = scan.next();
+            
+            
+            
+            System.out.println("Do you want to create another triangle? ('YES' or 'NO')");
+            loop_termination = scan.next();
+            
+        }while(loop_termination.equalsIgnoreCase("YES"));
+
     }
     
 }
