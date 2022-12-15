b871189cc10dacf38ec1553899a4442525445fee
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-12-15 18:48:27 +0200

    implemented another catch block

--- Objects/src/objects/Objects.java
+++ Objects/src/objects/Objects.java
@@ -27,33 +27,36 @@ public class Objects {
                     + "---------------------------------------------------------"
                     + "\n\nChoose color: ");
             String color = scan.next();
-            
-            try{
-                System.out.print("Choose isFilled('true or false): ");
-                boolean isFilled;
-                if(scan.next().equalsIgnoreCase("true")){
-                    isFilled = true;
-                }else{
-                    isFilled = false;
+
+                try{
+                    System.out.print("Choose isFilled('true or false): ");
+                    boolean isFilled;
+                    if(scan.next().equalsIgnoreCase("true")){
+                        isFilled = true;
+                    }else{
+                        isFilled = false;
+                    }
+
+                    System.out.print("Choose side1:");
+                    double s1 = scan.nextDouble();
+                    System.out.print("Choose side2:");
+                    double s2 = scan.nextDouble();
+                    System.out.print("Choose side3:");
+                    double s3 = scan.nextDouble();
+
+
+                    Triangle new_t = new Triangle(s1, s2, s3, color, isFilled);
+                    System.out.println(new_t.toString());
+
+                }catch(InputMismatchException e){
+
+                    Class classX = (scan.next()).getClass();
+                    System.out.println(e + "  =>  " +classX);
+
+                }catch(Exception e){
+                    System.out.println("Something went wrong \n"+e);
                 }
-
-                System.out.print("Choose side1:");
-                double s1 = scan.nextDouble();
-                System.out.print("Choose side2:");
-                double s2 = scan.nextDouble();
-                System.out.print("Choose side3:");
-                double s3 = scan.nextDouble();
-
-
-                Triangle new_t = new Triangle(s1, s2, s3, color, isFilled);
-                System.out.println(new_t.toString());
-
-            }catch(InputMismatchException e){
-              
-                Class classX = (scan.next()).getClass();
-                System.out.println(e + "  =>  " +classX);
                 
-            }
             
             System.out.println("Do you want to create another triangle? ('YES' or 'NO')");
             loop_termination = scan.next();
