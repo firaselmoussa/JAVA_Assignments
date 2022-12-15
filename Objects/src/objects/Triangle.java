9dc7093cfb60166cd9e4885697f9b4b89de4eeff
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-12-15 18:24:24 +0200

    implemented no-args constructor

--- Objects/src/objects/Triangle.java
+++ Objects/src/objects/Triangle.java
@@ -11,4 +11,11 @@ package objects;
 public class Triangle {
     private double side1, side2, side3 = 1.0;
     private double area, perimeter;
+    
+    public Triangle() {
+        super();
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
 }
