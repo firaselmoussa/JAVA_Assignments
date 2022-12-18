def02e382a51592b791efcb3032cf3f4f9df0d8f
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-12-18 20:29:30 +0200

    created culculator frame

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -15,6 +15,10 @@ public class Calculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //CALCULATOR
+        new CalculatorFrame();
+        
     }
     
 }
