3ca537d0d4f5c0fbe204b8e932cd4e2d08c776b2
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2023-01-06 21:38:10 +0200

    implemented getters & setters

--- Pharmacy/src/pharmacy/Prescription.java
+++ Pharmacy/src/pharmacy/Prescription.java
@@ -17,6 +17,31 @@ public class Prescription extends Medicine{
         this.code = code;
     }
 
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    
     @Override
     public String toString() {
         return "Prescription{" + "code=" + code + '}';
