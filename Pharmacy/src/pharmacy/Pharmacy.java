8b9e054cf5c09c25fb3937365e8c8be6a79ac022
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2023-01-06 22:37:54 +0200

    implemented sample run

--- Pharmacy/src/pharmacy/Pharmacy.java
+++ Pharmacy/src/pharmacy/Pharmacy.java
@@ -15,6 +15,17 @@ public class Pharmacy {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Store store = new Store("Pharmacy", "street, city", 6);
+        Prescription m1 =  new Prescription("Med11", "Medicine", 29.9);
+        OverCounter o1 =  new OverCounter(true, "Medicine", 80);
+        
+        store.addMedicine(m1);
+        store.addMedicine(new Prescription("Med33", "Med", 65));
+        
+        System.out.println(o1.findPriceAfterTax() +"\n"+store.buyMedicine("Medicine"));
+       
+        
     }
     
 }
