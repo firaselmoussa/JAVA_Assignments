939234cbb82ab1579eb7b5db7c362b02332e2457
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2023-01-06 21:34:22 +0200

    implemented getters & setters

--- Pharmacy/src/pharmacy/Medicine.java
+++ Pharmacy/src/pharmacy/Medicine.java
@@ -18,6 +18,24 @@ public class Medicine {
         this.price = price;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    
+
     @Override
     public String toString() {
         return "Medicine{" + "name=" + name + ", price=" + price + '}';
