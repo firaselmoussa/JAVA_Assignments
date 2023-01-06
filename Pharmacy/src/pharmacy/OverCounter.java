11a639700b7d3b7f47d7631694527956d08cb2f2
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2023-01-06 21:45:26 +0200

    implemented new class & implemented all abstract methods

---
+++ Pharmacy/src/pharmacy/OverCounter.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pharmacy;
+
+/**
+ *
+ * @author Firas
+ */
+public class OverCounter extends Medicine implements Taxable{
+
+    @Override
+    public double findPriceAfterTax() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
