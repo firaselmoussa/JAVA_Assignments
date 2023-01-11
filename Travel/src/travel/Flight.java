24bc31b4956d6b45ae34b17249174e9469d9a1d1
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2023-01-11 19:04:27 +0200

    implemented all abstract methods

--- Travel/src/travel/Flight.java
+++ Travel/src/travel/Flight.java
@@ -8,6 +8,18 @@ package travel;
  *
  * @author Firas
  */
-public class Flight {
+public class Flight extends Trip implements Economize{
+    private String seatClass;
+
+    @Override
+    public double calculateCost() {
+        
+    }
+
+    @Override
+    public void economize() {
+        
+    }
+    
     
 }
