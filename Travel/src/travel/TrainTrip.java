3e0d3e2a6af04b13378f921e5cc1aa5ff8cd5a4a
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2023-01-11 17:11:55 +0200

    implemented data fields & constructor

--- Travel/src/travel/TrainTrip.java
+++ Travel/src/travel/TrainTrip.java
@@ -8,6 +8,28 @@ package travel;
  *
  * @author Firas
  */
-public class TrainTrip {
+public class TrainTrip extends Trip implements Economize{
+    private boolean isVIP;
+
+    public TrainTrip(boolean isVIP, String departureCity, String arrivalCity, double basicPrice) {
+        super(departureCity, arrivalCity, basicPrice);
+        this.isVIP = isVIP;
+    }
+    
+  
+
+    @Override
+    public double calculateCost() {
+        
+        return 0;
+        
+    }
+
+    @Override
+    public void economize() {
+        
+    }
+
+   
     
 }
