bebc61ccc89a8ae0c895651eab51e1cfbac28645
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2023-01-11 19:01:09 +0200

    implemented toString method & added comments

--- Travel/src/travel/TrainTrip.java
+++ Travel/src/travel/TrainTrip.java
@@ -9,8 +9,10 @@ package travel;
  * @author Firas
  */
 public class TrainTrip extends Trip implements Economize{
+    //DATA FIELDS
     private boolean isVIP;
 
+    //CONSTRUCTOR
     public TrainTrip(boolean isVIP, String departureCity, String arrivalCity, double basicPrice) {
         super(departureCity, arrivalCity, basicPrice);
         this.isVIP = isVIP;
@@ -18,6 +20,7 @@ public class TrainTrip extends Trip implements Economize{
     
   
 
+    //ABSTRACT, CALCULATE COST
     @Override
     public double calculateCost() {
         double cost = this.getBasicPrice();
@@ -28,11 +31,23 @@ public class TrainTrip extends Trip implements Economize{
         
     }
 
+    //ABSTRACT, ECONOMIZE
     @Override
     public void economize() {
         isVIP = false;
     }
 
+    //TO STRING
+    @Override
+    public String toString() {
+        String s = "A train trip departing from "+this.getDepartureCity()+" to "+this.getArrivalCity()+" for a basic price of 270$.";
+        if(isVIP)
+            s += "\nIt is a VIP reservation.";
+        else
+            s += "\nIt is NOT a VIP reservation.";
+        
+        return s;
+    }
+
    
-    
 }
