ed8ca6f13a8d0960d8a4c79967e2897d79ddfa42
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2023-01-15 11:53:16 +0200

    implemented interface

---
+++ Programs/src/programs/Language.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package programs;
+
+/**
+ *
+ * @author Firas
+ */
+public interface Language {
+    public String getType();
+    public int getDateOfRelease();
+    public String getName();
+}
